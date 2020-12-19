e3ec81784b59f985383d7e8220a29a373cb7b7ce
Xin Ren <renxin714@outlook.com>
2020-12-19 10:36:37 +0800

    rename Solutions/ to solutions/

--- main_include.h
+++ main_include.h
@@ -2,20 +2,20 @@
 #define TTLEETCODE_MAIN_INCLUDE_H
 
 #if 0
-#include "Solutions/testSolution.h"
-#include "Solutions/q148_sort_list.h"
-#include "Solutions/q237_delete_node_in_a_linked_list.h"
-#include "Solutions/q328_odd_even_linked_list.h"
-#include "Solutions/q138_copy_list_with_random_pointer.h"
-#include "Solutions/q103_binary_tree_zigzag_level_order_traversal.h"
-#include "Solutions/q104_maximum_depth_of_binary_tree.h"
-#include "Solutions/q105.h"
-#include "Solutions/q108_convert_sorted_array_to_.h"
-#include "Solutions/q124_binary_tree_maximum_path_sum.h"
+#include "solutions/testSolution.h"
+#include "solutions/q148_sort_list.h"
+#include "solutions/q237_delete_node_in_a_linked_list.h"
+#include "solutions/q328_odd_even_linked_list.h"
+#include "solutions/q138_copy_list_with_random_pointer.h"
+#include "solutions/q103_binary_tree_zigzag_level_order_traversal.h"
+#include "solutions/q104_maximum_depth_of_binary_tree.h"
+#include "solutions/q105.h"
+#include "solutions/q108_convert_sorted_array_to_.h"
+#include "solutions/q124_binary_tree_maximum_path_sum.h"
 #endif
 
 #if 1
-#include "Solutions/q230_kth_smallest_element_in_a_bst.h"
+#include "solutions/q230_kth_smallest_element_in_a_bst.h"
 #endif
 
 #if 0
